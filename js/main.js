2417481c6189697f8d87f40daea302e622c5d167
kairov2605 <kairov2605@gmail.com>
2020-12-15 23:09:55 +0600

    Основная верстка

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="arrow arrow__prev"><img src="img/left.svg" alt=""></button>',
+        nextArrow: '<button class="arrow arrow__next"><img src="img/right.svg" alt=""></button>',
+    });
+});
